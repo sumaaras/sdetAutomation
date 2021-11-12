7c45c84fba3b3aa8f68f1aa527ac58b0e594d9f9
sumaaras <suma.aras@gmail.com>
2021-11-12 13:38:21 +0530

    new commit

---
+++ src/test/java/com/practiceTest/Sample1Test.java
@@ -0,0 +1,12 @@
+package com.practiceTest;
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+	@Test
+	public void simpleTest() {
+		System.out.println("hiiii.user1 first comment");
+	}
+	
+
+}
